9c8cb04a7eb080240fccf19ee9a386f4760a3e3f
jeephalin <jeephalin@hotmail.com>
2016-12-01 19:38:06 +0800

    0

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(Convert.ToInt32(DateTime.Now.ToString("ddHHmmssff")));
+        }
+    }
+}
